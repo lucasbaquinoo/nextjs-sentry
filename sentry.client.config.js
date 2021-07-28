612adf53e90fae8299bfc6e8a63d762608afff04
lucasbaquinoo <lucasbaquino@icloud.com>
2021-07-27 22:30:13 -0300

    create the .env

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -3,9 +3,7 @@ import * as Sentry from '@sentry/nextjs';
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn:
-    SENTRY_DSN ||
-    'https://052bb01c5cba41ea9855414af58c897c@o930263.ingest.sentry.io/5878826',
+  dsn: SENTRY_DSN,
   // We recommend adjusting this value in production, or using tracesSampler
   // for finer control
   tracesSampleRate: 1.0,
